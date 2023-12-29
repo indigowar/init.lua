59933dd37f1e02a652f495157a31ad4b7d0e4ea6
Max Kamerov <indigowar.github@gmail.com>
2023-12-29 21:23:46 +0300

    added lualine, modified telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,14 @@
+require('telescope').setup({
+    pickers = {
+        find_files = {
+            -- theme = 'dropdown',
+        },
+    },
+})
+
+
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
--- lua/indigowar/lazy.lua
+++ lua/indigowar/lazy.lua
@@ -35,5 +35,8 @@ require('lazy').setup({
         },
         config = true
     },
-
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
 })
